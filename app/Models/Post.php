ea1394a2cfa63e8a359e0c4f527576bf4b6ccbac
momoshin117 <shintaro.cm@gmail.com>
2024-07-30 21:40:04 +0900

    課題8-6-2

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 
 class Post extends Model
@@ -13,6 +14,8 @@ class Post extends Model
     'body',
     ];
     use HasFactory;
+    use SoftDeletes;
+    
     public function getPaginateByLimit(int $limit_count=5){
         return $this ->orderby('updated_at','DESC')->paginate($limit_count);
     }
